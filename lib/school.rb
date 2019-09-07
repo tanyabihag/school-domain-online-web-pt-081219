67750ee9b5d250ad4a9208e18c5039a50b99e802
Tanya Bihag <tbihag@gmail.com>
2019-09-07 23:15:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,3 @@
 class School
-  attr_accessor :name, :roster
-   
-   def initialize(school_name)
-     @name = school_name
-     @roster = {}
-    end
-    
-    def add_student(student_name, student_gade)
-      if @roster[student_grade] == nil
-         @roster[student_grade] = [student_name]
-       else
-        @roster[student_grade] << student_name
-    end
-  end
-end
+  
+  def inistialize
